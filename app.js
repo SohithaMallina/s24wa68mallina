15ebe89be2aab7e0c1b554900e3b74d4a701fc38
Sohitha Mallina <110340026+SohithaMallina@users.noreply.github.com>
2024-10-30 15:01:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter =require('./routes/mydata');
 var app = express();
-const computationRouter = require('./computation');
+var computationRouter = require('./routes/computation');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
