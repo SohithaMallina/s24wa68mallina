01203be12bf5333659cae723a98b795348984010
SohithaMallina <sohithamallina12@gmail.com>
2024-10-30 14:42:30 -0500

    added computation

---
+++ computation.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/computation', (req, res) => {
+    const x = req.query.x ? parseFloat(req.query.x) : Math.random();
+    const result1 = Math.atan(x);
+    const result2 = Math.exp(x);
+    const result3 = Math.expm1(x);
+    const response = `Math.atan applied to ${x} is ${result1}\nMath.exp applied to ${x} is ${result2}\nMath.expm1 applied to ${x} is ${result3}`;
+    res.send(response);
+});
+
+module.exports = router;
