6091d9c7126d589bd36fe783ba65f37e16ba25e6
Sohitha Mallina <110340026+SohithaMallina@users.noreply.github.com>
2024-10-30 15:27:13 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET mydata page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sohitha Mallina' });
+router.get('/computation', (req, res) => {
+    const x = req.query.x ? parseFloat(req.query.x) : Math.random();
+    const result1 = Math.atan(x);
+    const result2 = Math.exp(x);
+    const result3 = Math.expm1(x);
+    const response = `Math.atan applied to ${x} is ${result1}\nMath.exp applied to ${x} is ${result2}\nMath.expm1 applied to ${x} is ${result3}`;
+    res.send(response);
 });
 
 module.exports = router;
