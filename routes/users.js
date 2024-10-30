f3e562c932740c1bcdab4c17f64feddd7b1b09aa
SohithaMallina <sohithamallina12@gmail.com>
2024-10-30 13:18:29 -0500

    added

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+let value = 0;
+let total = 1;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  value += 2;
+  total += value;
+  res.send(`Total is: ${total}`);
   res.send('respond with a resource');
 });
 
